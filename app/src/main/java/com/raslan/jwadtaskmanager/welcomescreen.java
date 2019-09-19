ae63f5f4beab4130df741085d046c51f8058aaa4
moaad <rakaz.hammad12@gmail.com>
2019-09-19 14:07:24 +0300

    fgfjg

--- app/src/main/java/com/raslan/jwadtaskmanager/welcomescreen.java
+++ app/src/main/java/com/raslan/jwadtaskmanager/welcomescreen.java
@@ -2,6 +2,7 @@ package com.raslan.jwadtaskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class welcomescreen extends AppCompatActivity {
@@ -10,5 +11,20 @@ public class welcomescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcomescreen);
+        Thread th=new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(3*1000);
+                    Intent i=new Intent(getApplication(),Signin.class);
+                    startActivity(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        th.start();
     }
 }
+
