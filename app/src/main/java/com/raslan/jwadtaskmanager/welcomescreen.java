feec04ec79ed8bca7574647fcf81cac48c48cb3f
moaad <rakaz.hammad12@gmail.com>
2019-10-08 12:22:53 +0300

    signup to sign in

--- app/src/main/java/com/raslan/jwadtaskmanager/welcomescreen.java
+++ app/src/main/java/com/raslan/jwadtaskmanager/welcomescreen.java
@@ -5,26 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class welcomescreen extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcomescreen);
+    protected void onResume() {
+        super.onResume();
         Thread th=new Thread()
         {
             @Override
             public void run() {
                 try {
                     sleep(3*1000);
-                    Intent i=new Intent(getApplication(),Signin.class);
-                    startActivity(i);
+                    FirebaseAuth auth =FirebaseAuth.getInstance();
+                    if (auth.getCurrentUser()==null||auth.getCurrentUser().getEmail()==null)
+                    {
+                        Intent i=new Intent(getApplication(),Signin.class);
+                        startActivity(i);
+                        finish();
+                    }
+                    else {
+
+
+                        Intent i = new Intent(getApplication(), Signin.class);
+                        startActivity(i);
+                        finish();
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
         th.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcomescreen);
+
     }
 }
 
