ee567735166ba69915c257d8d7b30d8ba276e2ce
moaad <rakaz.hammad12@gmail.com>
2019-10-27 12:19:21 +0200

    signup to sign in

--- app/src/main/java/com/raslan/jwadtaskmanager/AllTasksFragment.java
+++ app/src/main/java/com/raslan/jwadtaskmanager/AllTasksFragment.java
@@ -3,15 +3,22 @@ package com.raslan.jwadtaskmanager;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import data.Mytask;
 
 
 /**
@@ -35,9 +42,26 @@ public class AllTasksFragment extends Fragment {
     {
         FirebaseDatabase database=FirebaseDatabase.getInstance();
         FirebaseAuth auth=FirebaseAuth.getInstance();
-        String uid = auth.getUid();
+        String uid= auth.getUid();
         DatabaseReference reference = database.getReference();
-        reference
+        reference.child("tasks").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                for (DataSnapshot d: dataSnapshot.getChildren())   //data snapshot ns5a 3n jme3 almo3teat almojoda t7t uid
+                {
+                    Mytask t=d.getValue(Mytask.class);
+                    Log.d("Mytask",t.toString());
+
+                }
+            }
+            //data snapshot ns5a 3n jme3 almo3teat almojoda t7t uid
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
