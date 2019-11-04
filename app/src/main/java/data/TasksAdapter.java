12db96c4dfbb21b92e1782a07cc38275886dfec9
moaad <rakaz.hammad12@gmail.com>
2019-11-04 09:41:04 +0200

    signup to sign in

---
+++ app/src/main/java/data/TasksAdapter.java
@@ -0,0 +1,41 @@
+package data;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.raslan.jwadtaskmanager.R;
+
+public class TasksAdapter extends ArrayAdapter<Mytask> {
+
+    public TasksAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    @NonNull
+    @Override
+
+
+
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        View vitem= LayoutInflater.from(getContext()).inflate(R.layout.taskitem,true);
+        TextView tvTitle=vitem.findViewById(R.id.itmTvSubject);
+        TextView tvsubject=vitem.findViewById(R.id.itmTvSubject);
+        Image tvimage=vitem.findViewById(R.id.itmImdInfo);
+        CheckBox tvcheckbox=vitem.findViewById(R.id.itmChbxIsCompleted);
+        RatingBar tvratingbar=vitem.findViewById(R.id.itmRatingPrio);ss
+
+
+
+    }
+}
